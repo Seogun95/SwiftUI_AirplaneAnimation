a807870552491bb1bf9cbaecea3e09328981ef21
SeonJungKim <ksj083895@gmail.com>
2021-06-23 20:15:44 +0900

    Initial Commit

---
+++ SwiftUI_AirplaneAnimation/SwiftUI_AirplaneAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AirplaneAnimationApp.swift
+//  SwiftUI_AirplaneAnimation
+//
+//  Created by Seogun Kim on 2021/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AirplaneAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
